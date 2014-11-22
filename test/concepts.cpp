fbde0697accebf0550851b23de2ef1f8200ce382
Dmitry Ledentsov <dmlled@yahoo.com>
2014-11-22 10:38:15 +0100

    get all keys concept

--- test/concepts.cpp
+++ test/concepts.cpp
@@ -11,57 +11,75 @@ TEST_CASE("literals encoding strategy") {
 
 namespace {
 
-	class Resource /*final*/ {
-	public:
-		typedef std::string(*ResourceGetter)();
-	public:
-		static std::string Test();
-		static std::string PlainText();
-	public: // key/value api
-		static std::string Get(std::string const& key);
-	public:
-		static std::string OnNoKey() {
-			// can be throwing or not
-			return "";
-		}
-	};
+    class Resource /*final*/ {
+    public:
+        typedef std::string(*ResourceGetter)();
+    public:
+        static std::string Test();
+        static std::string PlainText();
+    public: // key/value api
+        template <typename TInserter>
+        static void GetKeys(TInserter& inserter) {
+            static const char* keys[] = {
+                "Test",
+                "PlainText"
+            };
+            for (auto key : keys)
+                inserter = key;
+        }
 
-	std::string Resource::Test() {
-		static char const literal[] = { 0, 1, 2, 0, 1 };
-		return std::string(literal, sizeof(literal)/sizeof(char));
-	}
+        static std::string Get(std::string const& key);
+    public:
+        static std::string OnNoKey() {
+            // can be throwing or not
+            return "";
+        }
+    };
 
-	std::string Resource::PlainText() {
-		static char const literal[] = "some plain text";
-		return std::string(literal);
-	}
+    std::string Resource::Test() {
+        static char const literal[] = { 0, 1, 2, 0, 1 };
+        return std::string(literal, sizeof(literal)/sizeof(char));
+    }
 
-	std::string Resource::Get(std::string const& key) {
-		static std::unordered_map<std::string,ResourceGetter> getters = {
-			{ "Test", Resource::Test },
-			{ "PlainText", Resource::PlainText },
-		};
+    std::string Resource::PlainText() {
+        static char const literal[] = "some plain text";
+        return std::string(literal);
+    }
 
-		auto getter = getters.find(key);
-		if (getter == getters.end())
-			return OnNoKey();
+    std::string Resource::Get(std::string const& key) {
+        static std::unordered_map<std::string,ResourceGetter> getters = {
+            { "Test", Resource::Test },
+            { "PlainText", Resource::PlainText },
+        };
 
-		return getter->second();
-	}
+        auto getter = getters.find(key);
+        if (getter == getters.end())
+            return OnNoKey();
+
+        return getter->second();
+    }
 }
 
 TEST_CASE("api") {
-	auto res=Resource::Test();
-	char const literal[] = { 0, 1, 2, 0, 1 };
-	std::string reference(literal, 5);
-	CHECK( reference == res );
-	std::string another(literal, 3);
-	CHECK( another != res );
+    auto res=Resource::Test();
+    char const literal[] = { 0, 1, 2, 0, 1 };
+    std::string reference(literal, 5);
+    CHECK( reference == res );
+    std::string another(literal, 3);
+    CHECK( another != res );
 
-	CHECK( Resource::PlainText() == "some plain text" );
+    CHECK( Resource::PlainText() == "some plain text" );
 
-	SECTION("key/value api") {
-		CHECK( Resource::Get("PlainText") == "some plain text" );
-		CHECK( Resource::Get("Whatever") == "" );
-	}
+    SECTION("key/value api") {
+        CHECK( Resource::Get("PlainText") == "some plain text" );
+        CHECK( Resource::Get("Whatever") == "" );
+    }
+
+    SECTION("getting all keys") {
+        std::vector<std::string> keys;
+        Resource::GetKeys(std::back_inserter(keys));
+        CHECK(keys.size() == 2);
+        CHECK(keys[0] == "Test");
+        CHECK(keys[1] == "PlainText");
+    }
 }
